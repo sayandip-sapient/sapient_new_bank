1d8516fd0b363cf8b3353c8e0100518d378138f4
sayachar <sayandip.acharjee@publicissapient.com>
2023-12-07 22:22:15 +0530

    banking system app updated with different banks

---
+++ src/ICICI.java
@@ -0,0 +1,85 @@
+public class ICICI implements RBI {
+    double balance;
+    int count=0;
+    double roi;
+    int year;
+
+
+
+    ICICI (){
+         balance=1000.0D;
+         roi=6.0D;
+          year=3;
+
+    }
+
+    @Override
+    public void deposit(double amount) {
+       if (amount >= 0) {
+           System.out.println(balance+"hiii");
+                balance += amount;
+                System.out.println("Deposit successful. Your new balance is: " + balance);
+            } else {
+                System.out.println("Invalid deposit amount. Please enter a non-negative value.");
+            }
+
+    }
+
+    @Override
+    public void withdraw(double withdrawAmount) {
+        count++;
+        if(count>3 && (withdrawAmount+((1*withdrawAmount)/100) <= balance-1000) ){
+                balance=balance-withdrawAmount-((1*withdrawAmount)/100);
+                System.out.println("Deposit successful. Your new balance is: " + balance);
+            }
+           else if (withdrawAmount <= balance-1000) {
+               balance=balance-withdrawAmount;
+                System.out.println("Deposit successful. Your new balance is: " + balance);
+            } else {
+                System.out.println("Invalid deposit amount. Please enter a non-negative value.");
+            }
+
+    }
+
+    @Override
+    public void createFixedDeposit(double amount, int durationInYears,double ROI) {
+        double interestRateDecimal = ROI / 100.0;
+
+        double maturityAmount = amount * Math.pow(1 + interestRateDecimal, durationInYears);
+        System.out.println(maturityAmount);
+        System.out.println("ICICI: Fixed Deposit of $" + amount + " for " + durationInYears + " years created.");
+    }
+
+    @Override
+    public void applyLoan(int loanType, double amount, int years) {
+        double rate;
+        switch (loanType) {
+            case 1: {
+                rate = 5;
+                break;
+            }
+            case 2: {
+                rate = 3;
+                break;
+            }
+            case 3: {
+                rate = 6;
+                break;
+            }
+            case 4: {
+                rate = 8;
+                break;
+            }
+            default: {
+                rate = roi;
+            }
+        }
+        double prinDeduction = amount / years;
+        System.out.println("Interest per year will be as following");
+        for (int i = 0; i < years; i++) {
+            System.out.println(amount * rate * 0.01);
+            amount -= prinDeduction;
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,31 @@ public class Main {
     int selectedBank, selectedOperation;
     public static void main(String[] args) {
         Main obj = new Main();
-        RBI depo =new RBI();
+//        RBI icici =new ICICI();
+//        RBI  =new ICICI();
+//        RBI depo =new ICICI();
+//        RBI depo =new ICICI();
+        RBI temp=null;
+
         System.out.println("Welcome to IBS\nPlease select your bank\n1. ICICI\n2. HDFC\n3. SBI\n4. AXIS\n5. IDFC");
 
         try {
             obj.selectedBank = Integer.parseInt(obj.buff.readLine());
+//            RBI req=new obj.selectedBank;
+
+            switch (obj.selectedBank){
+                case 1:
+                    temp = new ICICI();
+                    break;
+                case 2:
+                    temp = new HDFC();
+                    break;
+
+                default:
+                   System.out.println("wrong bank");
+
+            }
+
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -34,27 +54,43 @@ public class Main {
             }
             System.out.println("Customer Selected " + obj.selectedOperation);
 
-
+           BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             switch (obj.selectedOperation) {
                 case 1:
 
-                    depo.depositMoney();
+
+        try {
+            System.out.print("Enter the amount to deposit: ");
+            String input = reader.readLine();
+            double amount= Double.parseDouble(input);
+            temp.deposit(amount);
+
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Error reading input. Please enter a valid number.");
+        }
+
                     break;
                 case 2:
-                    // code to be executed if expression matches value2
-                    depo.withdrawMoney();
+                    try{
+                        System.out.print("Enter the amount to withdraw: ");
+                        String input = reader.readLine();
+                        double withdrawAmount = Double.parseDouble(input);
+                        temp.withdraw(withdrawAmount);
+                    }catch (IOException | NumberFormatException e){
+                        System.out.println("Error reading input. Please enter a valid number.");
+                    }
+
                     break;
 
                 case 3:
 
 
                     try {
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
                         System.out.print("Enter the amount to fd: ");
                         String input = reader.readLine();
                         float fdAmount = Float.parseFloat(input);
-                        depo.openFD(fdAmount,6.0F,3);
+                        temp.createFixedDeposit(fdAmount,2,6.0);
                     } catch (IOException | NumberFormatException e) {
                         System.out.println("Error reading input. Please enter a valid number.");
                     }
@@ -62,6 +98,24 @@ public class Main {
 
                     break ;
                 // more cases as needed
+                case 4:
+                    System.out.println("Select your choi1ce\n1. Home\n2. Education\n3. Personal\n4. Car");
+                    try {
+                        obj.selectedOperation = Integer.parseInt(obj.buff.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Customer Selected " + obj.selectedOperation);
+                    try{
+                        System.out.print("Enter the amount for loan: ");
+                        String input = reader.readLine();
+                        double loanAmount = Double.parseDouble(input);
+                        temp.applyLoan(obj.selectedOperation,loanAmount,4);
+                    }catch (IOException | NumberFormatException e){
+                        System.out.println("Error reading input. Please enter a valid number.");
+                    }
+                    break;
+
                 default:
                     break temp;
                     // code to be executed if expression doesn't match any case
